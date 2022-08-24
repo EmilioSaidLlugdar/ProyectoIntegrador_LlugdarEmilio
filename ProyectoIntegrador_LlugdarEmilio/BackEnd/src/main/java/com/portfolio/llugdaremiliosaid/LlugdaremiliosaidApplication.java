5b7a5bef3d6ecb0447a584851832f37fe183b40d
EmilioSaidLlugdar <emiliosaid90@gmail.com>
2022-08-24 01:00:19 -0300

    spring boot

---
+++ ProyectoIntegrador_LlugdarEmilio/BackEnd/src/main/java/com/portfolio/llugdaremiliosaid/LlugdaremiliosaidApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.llugdaremiliosaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlugdaremiliosaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlugdaremiliosaidApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_LlugdarEmilio/BackEnd/src/test/java/com/portfolio/llugdaremiliosaid/LlugdaremiliosaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.llugdaremiliosaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlugdaremiliosaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
