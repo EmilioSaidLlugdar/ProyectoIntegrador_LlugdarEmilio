1f9f22254ff73e50eb4d12a6f5e012e629df47f2
EmilioSaidLlugdar <emiliosaid90@gmail.com>
2022-09-08 16:19:07 -0300

    Login

--- ProyectoIntegrador_LlugdarEmilio/FrontEnd/src/app/app-routing.module.ts
+++ ProyectoIntegrador_LlugdarEmilio/FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*cunando sea localhost:4200 me dirige al http component (HomeComponent)
+  pero si es  localhost:4200/login me dirige al LoginComponent*/
+  {path:'',component: HomeComponent},
+  {path:'login',component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
