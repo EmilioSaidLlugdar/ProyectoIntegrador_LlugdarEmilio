ae12253d52d33da7a03a667c91fb9708b31b6e42
EmilioSaidLlugdar <emiliosaid90@gmail.com>
2022-10-05 18:50:06 -0300

    backend experiencia

---
+++ ProyectoIntegrador_LlugdarEmilio/BackEnd/src/main/java/com/portfolio/llugdaremiliosaid/Service/SExperiencia.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.llugdaremiliosaid.Service;
+
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Said
+ */
+
+@Service
+@Transactional //manentrdra la persistencia
+public class SExperiencia {
+    
+}
